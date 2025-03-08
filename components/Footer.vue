2dd68c01e4bdc02d1aabe8050a73de4bac975907
Nikolai Dorofeev - d0rich <dorich2000@gmail.com>
2025-03-08 20:07:24 +0400

    Enhance Footer component with feedback section and improved layout for better user interaction

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,15 +1,28 @@
 <template>
-  <footer
-    class="footer sm:footer-horizontal footer-center bg-primary text-primary-content p-4"
-  >
-    <aside>
-      <p>
-        Developer Ⓓ {{ new Date().getFullYear() }} - This website is developed
-        by
-        <a href="https://d0rich.me" class="link link-hover">
-          d0rich (Nikolai Dorofeev)
-        </a>
-      </p>
-    </aside>
+  <footer>
+    <div class="footer sm:footer-horizontal bg-base-200 text-base-content p-10">
+      <nav>
+        <h6 class="footer-title">Feedback</h6>
+        <a
+          href="https://github.com/d0rich/spotifly/issues"
+          target="_blank"
+          class="link link-hover"
+          >Report an issue</a
+        >
+      </nav>
+    </div>
+    <div
+      class="footer sm:footer-horizontal footer-center bg-primary text-primary-content p-4"
+    >
+      <aside>
+        <p>
+          Developer Ⓓ {{ new Date().getFullYear() }} - This website is developed
+          by
+          <a href="https://d0rich.me" class="link link-hover">
+            d0rich (Nikolai Dorofeev)
+          </a>
+        </p>
+      </aside>
+    </div>
   </footer>
 </template>
