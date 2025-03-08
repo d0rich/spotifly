c7c52b0d54d8bbba13104cd47ab7f315339b860e
Nikolai Dorofeev - d0rich <dorich2000@gmail.com>
2025-03-08 15:18:59 +0400

    Add alert components for improved status messaging in app.vue

--- app.vue
+++ app.vue
@@ -3,6 +3,9 @@ import { computed } from 'vue'
 import { useQueryControls } from './composables/useQueryControls'
 import SearchInput from './components/SearchInput.vue'
 import ItemCard from './components/ItemCard.vue'
+import AlertInfo from './components/AlertInfo.vue'
+import AlertError from './components/AlertError.vue'
+import AlertWarning from './components/AlertWarning.vue'
 
 const { searchQuery } = useQueryControls()
 
@@ -31,65 +34,18 @@ const items = computed(() => {
   <div class="container mx-auto">
     <SearchInput @search="execute" />
     <div class="mx-auto px-4 mt-10 max-w-5xl">
-      <div
-        v-if="status === 'idle' || status === 'pending'"
-        role="alert"
-        class="alert alert-info"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          class="h-6 w-6 shrink-0 stroke-current"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-          ></path>
-        </svg>
+      <AlertInfo v-if="status === 'idle' || status === 'pending'">
         <span v-if="status === 'idle'">Enter a search query.</span>
-        <span v-else
-          ><span class="loading loading-ring loading-md"></span>Loading...</span
-        >
-      </div>
-      <div v-else-if="error" role="alert" class="alert alert-error">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-6 w-6 shrink-0 stroke-current"
-          fill="none"
-          viewBox="0 0 24 24"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-          />
-        </svg>
+        <span v-else>
+          <span class="loading loading-ring loading-md" /> Loading...
+        </span>
+      </AlertInfo>
+      <AlertError v-else-if="error">
         <span>Error: something wrong happened on our side ({{ error }})</span>
-      </div>
-      <div
-        v-else-if="status === 'success' && items.length === 0"
-        role="alert"
-        class="alert alert-warning"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-6 w-6 shrink-0 stroke-current"
-          fill="none"
-          viewBox="0 0 24 24"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
-          />
-        </svg>
+      </AlertError>
+      <AlertWarning v-else-if="status === 'success' && items.length === 0">
         <span>No results</span>
-      </div>
+      </AlertWarning>
       <div v-else>
         <ItemCard
           class="my-4"
