b1bf3c9a90b8168604fc295cdfab7aa35702c916
Nikolai Dorofeev - d0rich <dorich2000@gmail.com>
2025-05-26 18:23:51 +0400

    Add cloudflare analytics via @nuxt/scripts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,12 +5,16 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ['~/assets/css/tailwind.css'],
+
   vite: {
     plugins: [tailwindcss()]
   },
+
   nitro: {
     prerender: {
       routes: ['/_d0rich/meta.json']
     }
-  }
+  },
+
+  modules: ['@nuxt/scripts']
 })
